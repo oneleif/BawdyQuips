4aad0373cbc4f660e447dda2b8edb7d10e41ae40
zmeriksen <zmeriksen@gmail.com>
2019-07-10 16:33:36 -0500

    Initial Commit

---
+++ BawdyQuips/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BawdyQuips
+//
+//  Created by Zach Eriksen on 7/10/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
